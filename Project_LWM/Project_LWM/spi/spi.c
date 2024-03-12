916096c9d3d132715406014f9bd5a51b913bcc70
Student <Student@PC-SC5053-07.utko.feec.vutbr.cz>
2024-03-12 10:48:29 +0100

    start

---
+++ Project_LWM/Project_LWM/spi/spi.c
@@ -0,0 +1,6 @@
+/*
+ * spi.c
+ *
+ * Created: 3/12/2024 10:47:54
+ *  Author: Student
+ */ 
---
+++ Project_LWM/Project_LWM/spi/spi.h
@@ -0,0 +1,16 @@
+/*
+ * spi.h
+ *
+ * Created: 3/12/2024 10:48:07
+ *  Author: Student
+ */ 
+
+
+#ifndef SPI_H_
+#define SPI_H_
+
+
+
+
+
+#endif /* SPI_H_ */
