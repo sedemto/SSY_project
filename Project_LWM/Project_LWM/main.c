29d10736e1acbdf3b890e51e7e0d76a5e4993fa9
Student <Student@PC-SC5053-07.utko.feec.vutbr.cz>
2024-03-12 10:32:14 +0100

    start

---
+++ Project_LWM/Project_LWM/main.c
@@ -0,0 +1,18 @@
+/*
+ * Project_LWM.c
+ *
+ * Created: 3/12/2024 10:30:31
+ * Author : Student
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
